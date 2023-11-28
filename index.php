82a8931c4c503b18ff76b0916cbba87f568801a8
Onivaldo Flores Junior <134303933+onivaldoflores@users.noreply.github.com>
2023-11-28 14:49:59 -0300

    inicio layout

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memórias no Papel</title>
+    <link rel="shortcut icon" href="img/ico.png" type="image/x-icon">
+    
+    <script src="js/scripts.js"></script>
+
+    <!--LINKS BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="estilo/estilo.css">
+
+    <!--SCRIPTS BOOTSTRAP-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    
+</head>
+<body>
+    <header class="container-sm container-xl row align-items-center justify-content-between">
+        <div class="col-4 justify-content-center">
+            <img src="img/logo-home.png" alt="Logo da empresa Memórias no Papel">
+        </div>
+        <div class="col-4">
+            <nav>
+                <ul>
+                    <a href="#">PLANOS</a>
+                    <a href="#">DÚVIDAS</a>
+                    <a href="#">CONTATO</a>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-4 row ">
+            ÍCONES DE REDES SOCIAIS
+         
+        </div>
+    </header>
+    <main class="container-sm container-xl row align-items-center justify-content-between">
+        <section class="banner ">
+            MAIN - banner
+        </section>
+        <section class="como-funciona ">
+            <h1>COMO FUNCIONA?</h1>
+            <p>O Memórias no Papel é um clube de assinatura de revelação de fotografias, onde as fotos podem ser reveladas a partir das suas redes sociais Facebook e Instagram ou você pode fazer o upload diretamente do seu celular ou computador. Após efetivar sua assinatura, você receberá as fotos no conforto da sua casa todo mês.</p>            
+        </section>
+        <section class="planos">
+            <div>
+                <h1>ESCOLHA SEU PLANO</h1>
+            </div>
+            <div class="plano-01">plano 01</div>
+            <div class="plano-02">plano 02</div>
+            <div class="plano-03">plano 03</div>
+        </section>
+        <section class="faq">
+            <h1>DÚVIDAS FREQUENTES</h1>
+        </section>
+        <section class="contato">
+            <h1>CONTATO</h1>
+        </section>
+    </main>
+    <footer class="container-sm container-xl row align-items-center justify-content-between">
+        RODAPÉ
+    </footer>
+
+
+    
+</body>
+</html>
